02c4af2213b031bf5f774f6b0f2632f8ba94fa28
monjo <jvmonjo@gmail.com>
2020-09-27 17:35:08 +0200

    fixed undefined

--- routes/webhooks/index.js
+++ routes/webhooks/index.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
   app.post('/webhook/change-claims', (req, res) => {
-    const isAdmin = req.body.event.new.isadmin
+    const isAdmin = req.body.event.data.new.isadmin
     console.log(isAdmin)
     return res.json({ data: { isAdmin: isAdmin } })
   })
